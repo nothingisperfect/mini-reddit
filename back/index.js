31ceda4620ebc6757d602d6843c0a42c6731d71e
nothingisperfect <enikitina_spb@mail.ru>
2022-04-14 23:42:07 +0300

    Option delete post

--- back/index.js
+++ back/index.js
@@ -1,10 +1,11 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { request, response } = require("express");
 
 app.use(cors())
 app.use(express.json())
@@ -35,6 +36,20 @@ app.post('/api/news', async (request, response) => {
 
         await news.insertOne(request.body)
 
+    } finally {
+        await client.close()
+    }
+})
+
+app.delete('/api/news/:id', async (request, response) => {
+    try {
+        await client.connect();
+
+        const database = client.db('perf-news')
+        const news = database.collection('news')
+
+        await news.deleteOne({_id : ObjectId(request.params.id)})
+        
     } finally {
         await client.close()
     }
--- front/src/components/NewsItem.js
+++ front/src/components/NewsItem.js
@@ -1,9 +1,16 @@
 import React from "react";
 import './NewsItem.css';
-
-// import { Link } from 'react-router-dom';
+import { AppState } from '../AppState';
+import ReactDOM from 'react-dom';
 
 function NewsItem({ news }) {
+    var clickHandler = async () => {
+        await fetch("http://localhost:3001/api/news/" + news._id, {
+            method: 'DELETE'
+        })
+        ReactDOM.unmountComponentAtNode(ReactDOM.findDOMNode(this).parentNode);
+    }
+
     return (
         <div className='news-item'>
             <div className='news-item__header'>
@@ -14,10 +21,12 @@ function NewsItem({ news }) {
                 className='news-item_image'
                 src={news.image}
                 alt={news.title}
-            />
-            }
-            
+            />}
             <p>{news.text}</p>
+            <div>
+                {AppState.nickname === news.author && 
+                <button className='button' type='button' onClick={clickHandler}>Удалить</button>}
+            </div>
         </div>
     );
 }
